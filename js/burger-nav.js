ccfd7ee55b4e3f1405810cea8bf940c2031b7b9a
Volodymyrdv <vovakryt123453@gmail.com>
2024-11-22 09:27:56 +0200

    aos

--- js/burger-nav.js
+++ js/burger-nav.js
@@ -1,22 +1,24 @@
 const manageBurger = document.getElementById('manage-burger');
 
-manageBurger.addEventListener("click", function () {
-    const menu = document.getElementById("nav-burger");
-    const img_burger = manageBurger.querySelector("img");
-    if (menu.style.display === "none") {
-        menu.style.display = "flex";
-        img_burger.src = "./../img/header/close.png"; 
-    } else {
-        menu.style.display = "none";
-        img_burger.src = "./../img/header/open.png";
-    }
+manageBurger.addEventListener('click', function () {
+	const menu = document.getElementById('nav-burger');
+	const img_burger = manageBurger.querySelector('img');
+	if (menu.style.display === 'none') {
+		menu.style.display = 'flex';
+		img_burger.src = './../img/header/close.png';
+	} else {
+		menu.style.display = 'none';
+		img_burger.src = './../img/header/open.png';
+	}
 });
 
-window.addEventListener("resize", function () {
-    const menu = document.getElementById("nav-burger");
-    const img_burger = manageBurger.querySelector("img");
-    if (window.innerWidth > 1200) {
-        menu.style.display = "none"; 
-        img_burger.src = "./img/header/open.png"; 
-    }
+window.addEventListener('resize', function () {
+	const menu = document.getElementById('nav-burger');
+	const img_burger = manageBurger.querySelector('img');
+	if (window.innerWidth > 1200) {
+		menu.style.display = 'none';
+		img_burger.src = './img/header/open.png';
+	}
 });
+
+AOS.init();
